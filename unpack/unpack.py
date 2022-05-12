66229a23fd56aefdb0a83003a1657f54c3a57448
Yiheng <development.henryquan@gmail.com>
2022-05-12 21:20:27 +1000

    unpacking app assets

--- unpack/unpack.py
+++ unpack/unpack.py
@@ -1,6 +1,7 @@
 import json
 import gnu_mo_files as mo
 import shutil
+import pathlib
 import os
 import sys
 from wows_gameparams.WoWsGameParams import WoWsGameParams
@@ -88,6 +89,103 @@ class WoWsUnpack:
 
         print("done decoding languages")
 
+    def _resetDir(self, dirname: str):
+        """
+        Removes a directory if it exists and creates a new one.
+        """
+        if os.path.exists(dirname):
+            shutil.rmtree(dirname)
+        os.mkdir(dirname)
+
+    def packAppAssets(self):
+        """
+        Packs assets for WoWs Info
+        """
+        gui_path = 'gui'
+        # TODO: to be updated when finalised
+        output_path = 'assets'
+        self._resetDir(output_path)
+        if not os.path.exists(gui_path):
+            raise FileNotFoundError("gui folder not found")
+
+        # ACHIEVEMENTS
+        self._resetDir(output_path + '/achievements')
+        for achievement in os.listdir(gui_path + '/achievements/icons'):
+            # remove grey icons and two placeholders
+            if achievement in ['icon_achievement.png', 'placeholder.png']:
+                continue
+            if '_des.png' in achievement:
+                continue
+
+            formatted_name = achievement.replace(
+                'icon_achievement_', ''
+            )
+            shutil.copy(
+                gui_path + '/achievements/icons/' + achievement,
+                output_path + '/achievements/' + formatted_name,
+            )
+
+        # SHIPS
+        self._resetDir(output_path + '/ships')
+        for ship in os.listdir(gui_path + '/ship_previews'):
+            if ship == 'placeholder.png':
+                continue
+
+            shutil.copy(
+                gui_path + '/ship_previews/' + ship,
+                output_path + '/ships/' + ship,
+            )
+
+        # UPGRADES
+        self._resetDir(output_path + '/upgrades')
+        for modernization in os.listdir(gui_path + '/modernization_icons'):
+            formatted_name = modernization.replace(
+                'icon_modernization_', ''
+            )
+            shutil.copy(
+                gui_path + '/modernization_icons/' + modernization,
+                output_path + '/upgrades/' + formatted_name,
+            )
+
+        # FLAGS
+        self._resetDir(output_path + '/flags')
+        for flag in os.listdir(gui_path + '/signal_flags'):
+            if '_des.png' in flag:
+                continue
+            shutil.copy(
+                gui_path + '/signal_flags/' + flag,
+                output_path + '/flags/' + flag,
+            )
+
+        # CAMOUFLAGES
+        self._resetDir(output_path + '/camouflages')
+        for camouflage in os.listdir(gui_path + '/camouflages'):
+            if not camouflage.startswith('PCEC'):
+                continue
+            if '_des.png' in camouflage:
+                continue
+            shutil.copy(
+                gui_path + '/camouflages/' + camouflage,
+                output_path + '/camouflages/' + camouflage,
+            )
+
+        # PERMOFLAGES
+        self._resetDir(output_path + '/permoflages')
+        for permoflage in os.listdir(gui_path + '/permoflages'):
+            if '_des.png' in permoflage:
+                continue
+            shutil.copy(
+                gui_path + '/\permoflages/' + permoflage,
+                output_path + '/\permoflages/' + permoflage,
+            )
+
+        # count the overall size of assets
+        root_directory = pathlib.Path(output_path)
+        assets_size = sum(
+            f.stat().st_size for f in root_directory.glob('**/*') if f.is_file()
+        ) / 1024 / 1024
+        print("done packing assets, size: {:.2f} MB".format(assets_size))
+
 
 if __name__ == "__main__":
     print("Make sure the game path is valid!")
@@ -97,11 +195,14 @@ if __name__ == "__main__":
         with open('game.path', 'r') as f:
             path = f.read()
         unpack = WoWsUnpack(path)
-        # unpack.unpackGameIcons()
-        # unpack.unpackGameMaps()
         # unpack.unpackGameParams()
-        unpack.decodeGameParams()
+        # unpack.decodeGameParams()
+
+        # unpack.unpackGameMaps()
         # unpack.decodeLanguages()
+
+        # unpack.unpackGameIcons()
+        unpack.packAppAssets()
     else:
         with open('game.path', 'w') as f:
             print("Created game.path")
